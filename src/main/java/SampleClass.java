52b7b0c42729788411c5008c986b56b79aa9385b
theo ferguson <fergy546@yahoo.com>
2023-03-06 17:44:34 +0000

    complete

--- src/main/java/SampleClass.java
+++ src/main/java/SampleClass.java
@@ -56,9 +56,14 @@ public class SampleClass {
     public boolean b;
 
     //    implement a custom .equals(SampleClass other){} method here.
+    public boolean equals(SampleClass other) {
+        return false;
+    }
 
 
     //    implement a custom .toString(){} method here.
-
+    public String toString() {
+        return "hello";
+    }
     
 }
